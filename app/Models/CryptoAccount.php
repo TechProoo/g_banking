81ef021513ce17294ce893081f2a8fa240e2fe1b
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2026-03-17 16:23:05 +0100

    Fix registration 500: add default values for CryptoAccount float columns

--- app/Models/CryptoAccount.php
+++ app/Models/CryptoAccount.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class CryptoAccount extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $attributes = [
+        'btc' => 0,
+        'eth' => 0,
+        'ltc' => 0,
+        'xrp' => 0,
+        'link' => 0,
+        'bat' => 0,
+        'aave' => 0,
+        'usdt' => 0,
+        'xlm' => 0,
+        'bch' => 0,
+    ];
 }
